8073a51a348b2e5cabbb46ebd68a242605e9a067
goteVagyok <foldesgeri@gmail.com>
2024-08-08 20:29:31 +0200

    dir init and gui shenanigans

--- src/pyserversync.py
+++ src/pyserversync.py
@@ -1,8 +1,48 @@
+import subprocess
+import sys
+
+from PyQt6.QtWidgets import QTableWidgetItem
+
+from job import Job
 from utils.utils import *
 from ui.main_window import app, call
 
+if os.geteuid() != 0:
+    # need sudo to edit the crontab file
+    print("sudo privileges are needed to modify some files.")
+    subprocess.call(['sudo', '.venv/bin/python3', *sys.argv])
+    sys.exit()
 
-if check_cronie_running() and check_rsync_available():
-    print("cronie and rsync are present")
+if not (check_cronie_running() and check_rsync_available()):
+    exit(1)
+
+print("cronie and rsync are present")
+init()
+
+SAVED_JOBS = load_saved_jobs()
+
+
+def save_sync():
+    job = Job()
+
+    job.name = call.sync_name_lineedit.text()
+    job.src = call.sync_src_lineedit.text()
+    job.dst = call.sync_dst_lineedit.text()
+
+    job.notifications = call.notification_cb.isChecked()
+    job.twoway = call.twoway_cb.isChecked()
+
+# call.sync_save_btn.clicked.connect(save_sync)
+
+# call.scheduled_jobs_table.setRowCount(1)
+# call.scheduled_jobs_table.setColumnCount(4)
+#
+# call.scheduled_jobs_table.setItem(0, 0, QTableWidgetItem("asd"))
+# call.scheduled_jobs_table.setItem(0, 1, QTableWidgetItem("asd"))
+# call.scheduled_jobs_table.setItem(0, 2, QTableWidgetItem("asd"))
+# call.scheduled_jobs_table.setItem(0, 3, QTableWidgetItem("asd"))
+#
+# call.scheduled_jobs_table.show()
+
 
 app.exec()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+
+from job import Job
 
 
 def alert(msg: str) -> None:
@@ -31,3 +34,41 @@ def check_rsync_available() -> bool:
     return True
 
 
+def init() -> None:
+    """
+    creates the /home/user/.pyserversync_jobs dir to store the generated files\n
+    :return: None
+    """
+    # get the name of the user even when running as sudo
+    name = subprocess.run(['logname'], stdout=subprocess.PIPE).stdout.decode('utf-8').removesuffix("\n")
+    # -p makes it idempotent
+    os.system(f"mkdir -p /home/{name}/.pyserversync_jobs")
+
+
+def load_saved_jobs() -> list[Job]:
+    files = os.listdir(".pyserversync_jobs")
+    with open("/etc/crontab", "r") as crontab:
+        cron_file = crontab.read()
+
+    # cron_file += "\ntesttt\n"
+    #
+    # with open("/etc/crontab", "w") as crontab:
+    #     crontab.write(cron_file)
+
+    # TODO this is ugly and not very fault tolerant. use regex or metadata files myb
+    # for f in files:
+    #     with open(f".pyserversync_jobs/{f}", "r") as scriptfile:
+    #         script = scriptfile.read()
+    #         lines = script.split("\n")
+    #         job = Job()
+    #         job.name = f.removesuffix(".sh")
+    #
+    #         args = lines[2].split(" ")
+    #         job.src = args[2]
+    #         job.dst = args[3]
+    #
+    #         job.notifications = "notify-send" in lines[-1]
+    #         job.twoway = lines[2].startswith("rsync") and lines[3].startswith("rsync")
+    #         job.enabled = None
+    #
+    #         job.script_path = f".pyserversync_jobs/{f}"
